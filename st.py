bcd68860c107aa4ebd5b1320e670cccfde47edb5
rahidz <rahidz2003@gmail.com>
2020-06-09 18:48:47 -0400

    Add files via upload

--- st.py
+++ st.py
@@ -6,25 +6,15 @@ import os
 
 CONTENT_IMAGE=str(sys.argv[1])
 STYLE_IMAGE=str(sys.argv[2])
-CONTENT_WEIGHT=int(sys.argv[3])
-STYLE_WEIGHT=int(sys.argv[4])
-STYLE_SCALE=int(sys.argv[5])
-ORIGINAL_COLORS=sys.argv[6]
-multipleimages=False
 
 content=Path(CONTENT_IMAGE).stem
 style=Path(STYLE_IMAGE).stem
-cw=str(CONTENT_WEIGHT)
-sw=str(STYLE_WEIGHT)
-scale=str(STYLE_SCALE)
-oc=str(ORIGINAL_COLORS)
 
-OUT_DIR=content+"_"+style+"_"+cw+"_"+sw+"_"+scale+"_"+oc
+OUT_DIR="strotss"+content+style
 os.makedirs(OUT_DIR, exist_ok=True)
 
-if ',' in STYLE_IMAGE:
-    multipleimages=True
-	
+print("python3 strotss.py --weight 0.25 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+
 os.system("python3 strotss.py --weight 0.25 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
 os.system("python3 strotss.py --weight 0.50 --output " + OUT_DIR + "/out50.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
