b7b78260a4a354879b2ad0a971fdd23f0d06b30f
rahidz <rahidz2003@gmail.com>
2020-06-11 17:20:21 -0400

    Add files via upload

--- st.py
+++ st.py
@@ -10,15 +10,15 @@ STYLE_IMAGE=str(sys.argv[2])
 content=Path(CONTENT_IMAGE).stem
 style=Path(STYLE_IMAGE).stem
 
-OUT_DIR="strotss"+content+style
+OUT_DIR="MyDrive/strotss/"+content+style
 os.makedirs(OUT_DIR, exist_ok=True)
 
-print("python3 strotss.py --weight 0.25 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+print("python3 strotss.py --weight 0.25 --resize_to 1024 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
-os.system("python3 strotss.py --weight 0.25 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+os.system("python3 strotss.py --weight 0.25 --resize_to 1024 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
-os.system("python3 strotss.py --weight 0.50 --output " + OUT_DIR + "/out50.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+os.system("python3 strotss.py --weight 0.50 --resize_to 1024 --output " + OUT_DIR + "/out50.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
-os.system("python3 strotss.py --weight 0.75 --output " + OUT_DIR + "/out75.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+os.system("python3 strotss.py --weight 0.75 --resize_to 1024 --output " + OUT_DIR + "/out75.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
-os.system("python3 strotss.py --weight 1.00 --output " + OUT_DIR + "/out100.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+os.system("python3 strotss.py --weight 1.00 --resize_to 1024 --output " + OUT_DIR + "/out100.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
