2baa777d4dde8614722f3701710819541cfce34c
rahidz <rahidz2003@gmail.com>
2020-06-14 04:01:12 -0400

    Add files via upload

--- st.py
+++ st.py
@@ -13,12 +13,10 @@ style=Path(STYLE_IMAGE).stem
 OUT_DIR="MyDrive/strotss/"+content+style
 os.makedirs(OUT_DIR, exist_ok=True)
 
-print("python3 strotss.py --weight 0.25 --resize_to 1024 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
-
-os.system("python3 strotss.py --weight 0.25 --resize_to 1024 --output " + OUT_DIR + "/out25.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
-
 os.system("python3 strotss.py --weight 0.50 --resize_to 1024 --output " + OUT_DIR + "/out50.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
 os.system("python3 strotss.py --weight 0.75 --resize_to 1024 --output " + OUT_DIR + "/out75.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
+
+os.system("python3 strotss.py --weight 0.85 --resize_to 1024 --output " + OUT_DIR + "/out85.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
 
 os.system("python3 strotss.py --weight 1.00 --resize_to 1024 --output " + OUT_DIR + "/out100.png " + CONTENT_IMAGE + " " + STYLE_IMAGE)
