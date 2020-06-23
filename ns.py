daa9c9fe45cc0f48d13fbc279510c024781d9c38
rahidz <rahidz2003@gmail.com>
2020-06-23 16:59:08 -0400

    Add files via upload

--- ns.py
+++ ns.py
@@ -40,7 +40,7 @@ if colorTransfer == True:
     os.system("python3 linear-color-transfer.py --target_image " + PREV + " --source_image " + STYLE_IMAGE + " --mode chol --output_image " + OUT_DIR + "/histstyleX0.png")
     PREV=OUT_DIR+"/histstyleX0.png"
 
-os.system("python3 neural_style.py -content_image " + CONTENT_IMAGE + " -style_image " + STYLE_IMAGE + " -init image -init_image " + PREV + " -style_scale " + STYLE_SCALE + " -print_iter 1000 -save_iter 0 -num_iterations 1000 -content_weight " + CONTENT_WEIGHT + " -style_weight " + STYLE_WEIGHT + " -image_size 512 -model_file Mydrive/models/nyud-fcn32s-color-heavy.pth -content_layers relu1_1,relu2_1,relu3_1,relu4_1,relu5_1 -style_layers relu1_1,relu2_1,relu3_1,relu4_1,relu5_1 -output_image " + OUT_DIR +"/X1.png -tv_weight 0.00001 -gpu 0 -backend cudnn -cudnn_autotune")
+os.system("python3 neural_style.py -content_image " + CONTENT_IMAGE + " -style_image " + STYLE_IMAGE + " -init image -init_image " + PREV + " -style_scale " + STYLE_SCALE + " -print_iter 1000 -save_iter 0 -num_iterations 1000 -content_weight " + CONTENT_WEIGHT + " -style_weight " + STYLE_WEIGHT + " -image_size 512 -model_file MyDrive/models/nyud-fcn32s-color-heavy.pth -content_layers relu1_1,relu2_1,relu3_1,relu4_1,relu5_1 -style_layers relu1_1,relu2_1,relu3_1,relu4_1,relu5_1 -output_image " + OUT_DIR +"/X1.png -tv_weight 0.00001 -gpu 0 -backend cudnn -cudnn_autotune")
 
 PREV=OUT_DIR+"/X1.png"
 
